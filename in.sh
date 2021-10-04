874c26e2c28b166398990b59a508ee9a47292213
raon <91876178+E22EEE@users.noreply.github.com>
2021-10-04 15:55:30 +0300

    Update and rename ran.sh to in.sh

--- in.sh
+++ in.sh
@@ -3,12 +3,12 @@ rm -rf $HOME/.telegram-cli
 install() {
 rm -rf $HOME/.telegram-cli
 sudo chmod +x tg
-chmod +x raon
+chmod +x raon1
 chmod +x Run
 ./Run
 }
 if [ "$1" = "ok" ]; then
 install
 fi
-chmod +x inl.sh
-lua inll.lua
+chmod +x install.sh
+lua install.lua
